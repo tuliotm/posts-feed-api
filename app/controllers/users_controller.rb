f9e0f9d45ceb3909d73a45bcc050bbc03fbe5546
tuliotm <tulioteodoromanso@gmail.com>
2023-08-11 21:55:48 -0300

    Root to update User route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: { user: @user }
     else
       render json: @user.errors, status: :unprocessable_entity
     end
